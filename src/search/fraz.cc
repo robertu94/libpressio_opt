4fe52c3d7409167587f2d0b5e6fcd6e7e456e62c
Juniper Tyree <juniper.tyree@helsinki.fi>
2026-07-05 05:29:26 +0300

    patch fraz for single-threaded wasm

--- src/search/fraz.cc
+++ src/search/fraz.cc
@@ -74,7 +74,7 @@ struct fraz_search: public pressio_search_plugin {
       pressio_search_results results;
       dlib::function_evaluation best_result;
       std::map<pressio_search_results::input_type, pressio_search_results::output_type> cache;
-      dlib::thread_pool pool((thread_safe) ? (nthreads): (1));
+      // dlib::thread_pool pool((thread_safe) ? (nthreads): (1));
       std::vector<dlib::function_evaluation> evaluations;
       try{
         evaluations = data_to_evaluations(evaluations_data, lower_bound.size());
@@ -138,7 +138,7 @@ struct fraz_search: public pressio_search_plugin {
 
             if(!skip) {
               best_result = dlib::find_min_global(
-                  pool,
+                  // pool,
                   fraz,
                   vector_to_dlib(lower_bound),
                   vector_to_dlib(upper_bound),
@@ -192,7 +192,7 @@ struct fraz_search: public pressio_search_plugin {
 
             if(!skip) {
               best_result = dlib::find_min_global(
-                  pool,
+                  // pool,
                   fraz,
                   vector_to_dlib(lower_bound),
                   vector_to_dlib(upper_bound),
@@ -232,7 +232,7 @@ struct fraz_search: public pressio_search_plugin {
 
             if(!skip) {
               best_result = dlib::find_max_global(
-                  pool,
+                  // pool,
                   fraz,
                   vector_to_dlib(lower_bound),
                   vector_to_dlib(upper_bound),
